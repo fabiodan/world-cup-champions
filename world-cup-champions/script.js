143a98e84c8ff134b46f8612b3c9346ae6233e6d
Fabio Dan <dan.fabio@yahoo.com>
2011-07-16 14:52:52 -0300

    Refactoring all code to an object with an init method.

--- world-cup-champions/script.js
+++ world-cup-champions/script.js
@@ -1,101 +1,85 @@
+var worldCupChampions = {
+	init : function() {
+		this.getJson();
+	},
+	getJson : function() {
+	    var head = document.querySelector("head") || document.documentElement,
+	        script = document.createElement("script");
 
-// To do: Refactoring all code in one object with a "init" call. 
-function getJson() {
-    var head = document.querySelector("head") || document.documentElement,
-        script = document.createElement("script");
+		/* 
+		 * YQL Query. Permalink: http://y.ahoo.it/W87GoSBp 
+		 * Source: http://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals
+	     */
+		script.src = "http://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%3D%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FList_of_FIFA_World_Cup_finals%22%20and%20xpath%3D'%2F%2Ftable%5B2%5D%2Ftr%2Ftd%5Bposition()%3C3%5D%2Fa'&format=json&callback=worldCupChampions.formatData";
+	    head.appendChild(script);
+		head.removeChild(script);
+	},
+	formatData : function(response) {
+	    var json = response.query.results.a,
+	        data = {};
 
-	// YQL Query. Source: http://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals
-    script.src = "http://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%3D%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FList_of_FIFA_World_Cup_finals%22%20and%20xpath%3D'%2F%2Ftable%5B2%5D%2Ftr%2Ftd%5Bposition()%3C3%5D%2Fa'&format=json&callback=formatData";
-    head.appendChild(script);
-	head.removeChild(script);
-}
-
-function formatData(response) {
-    var json = response.query.results.a,
-        data = {};
-
-    for (var i in json) {
-        if (i % 2 == 1) {
-            data[json[i - 1]] = json[i];
-        } 
-    }
-
-	buildGraph(
-	    data,
-	    {
-	        "Brazil" : "green",
-	        "Italy" : "blue"
+	    for (var i in json) {
+	        (i % 2 == 1) && (data[json[i - 1]] = json[i]);
 	    }
-	);
-}
 
-var graph = document.getElementById("graph");
-var ctx = graph.getContext("2d");
+		this.buildGraph(data, {"Brazil" : "green", "Italy" : "blue"});
+	},
 
-// To do: generate the elements dinamically relying on json data.
-var years = document.querySelectorAll("#years li");
-var teams = document.querySelectorAll("#teams li");
+	/*
+	 * Random Color Generator
+	 * http://www.redips.net/javascript/random-color-generator/
+	 */
+	randomColor : function() {
+	    var hex = '0123456789ABCDEF'.split(''),
+	        color = '#', i;
+	    for (i = 0; i < 6 ; i++) {
+	        color = color + hex[Math.floor(Math.random() * 16)];
+	    }
+	    return color;
+	},
+	buildGraph : function(data, teamsColors) {
+		var graph = document.getElementById("graph"),
+			ctx = graph.getContext("2d"),
+			years = document.querySelectorAll("#years li"), // To do: generate the elements dinamically relying on json data.
+			teams = document.querySelectorAll("#teams li"),
+			graphHeight = graph.offsetHeight,
+	    	memo = teamsColors || {};
 
-/*
- * Random Color Generator
- * http://www.redips.net/javascript/random-color-generator/
- */
-function randomColor() {
-    var hex = '0123456789ABCDEF'.split(''),
-        color = '#', i;
-    for (i = 0; i < 6 ; i++) {
-        color = color + hex[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+	    for (var i = 0; i < years.length; i++) {
 
-function buildGraph(data, teamsColors) {
-    var memo = teamsColors || {};
+	        // Getting the "year" element coordinates.
+	        var year = years[i].firstChild.nodeValue,
+	            yearLeftPos = years[i].offsetLeft,
+	            yearRightPos = yearLeftPos + years[i].offsetWidth;
 
-    for (var i = 0; i < years.length; i++) {
+	        for (var j = 0; j < teams.length; j++) {        
+	            var team = teams[j].firstChild.nodeValue;
 
-        // Getting the coordinates.
-        var year = years[i].firstChild.nodeValue,
-            graphHeight = graph.offsetHeight,
-            yearLeftPos = years[i].offsetLeft,
-            yearRightPos = yearLeftPos + years[i].offsetWidth;
+	            // Setting one color for each team.
+	            !memo[team] && (memo[team] = this.randomColor());
 
-        for (var j = 0; j < teams.length; j++) {        
-            var team = teams[j].firstChild.nodeValue;
-            
-            // Setting one color for each team.
-            !memo[team] && (memo[team] = randomColor());
-            
-            if (team == data[year]) {
-                var teamLeftPos = teams[j].offsetLeft,
-                	teamRightPos = teamLeftPos + teams[j].offsetWidth;
-    
-                // Initializing graph properties.
-                ctx.fillStyle = memo[team];
-                ctx.globalAlpha = 0.3;
-                
-                // Drawing graph.
-                ctx.beginPath();
-                ctx.moveTo(yearLeftPos, 0);
-                ctx.lineTo(yearRightPos, 0);
-                ctx.quadraticCurveTo(graphHeight/2, graphHeight/2, teamRightPos, graphHeight);
-                ctx.lineTo(teamLeftPos, graphHeight);
-                ctx.quadraticCurveTo(graphHeight/2, graphHeight/2, yearLeftPos, 0);
-                ctx.fill();
-            }
-        }
+	            if (team == data[year]) {
 
-        // Debugging.
-        /*
-        console.log("data[year]: " + data[year]);
-        console.log("yearLeft: " + yearLeftPos);
-        console.log("yearRight: " + yearRightPos);
-        console.log("teamLeft: " + teamLeftPos);
-        console.log("teamRight: " + teamRightPos);
-        console.log("graphHeight: " + graphHeight);
-        console.log(memo);
-        */
-    }
+			        // Getting the "team" element coordinates.
+	                var teamLeftPos = teams[j].offsetLeft,
+	                	teamRightPos = teamLeftPos + teams[j].offsetWidth;
+
+	                // Initializing graph properties.
+	                ctx.fillStyle = memo[team];
+	                ctx.globalAlpha = 0.3;
+
+	                // Drawing the graph.
+	                ctx.beginPath();
+	                ctx.moveTo(yearLeftPos, 0);
+	                ctx.lineTo(yearRightPos, 0);
+	                ctx.quadraticCurveTo(graphHeight/2, graphHeight/2, teamRightPos, graphHeight);
+	                ctx.lineTo(teamLeftPos, graphHeight);
+	                ctx.quadraticCurveTo(graphHeight/2, graphHeight/2, yearLeftPos, 0);
+	                ctx.fill();
+	            }
+	        }
+	    }		
+	}
 };
 
-getJson();
+worldCupChampions.init();
